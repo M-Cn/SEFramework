3e286e44fbc74de8d3a26f0fd7f1e40484b3aa66
Radai <consoli.1941284@studenti.uniroma1.it>
2026-09-07 13:32:50 +0200

    Update Simulation.h

--- framework/Simulation/Simulation.h
+++ framework/Simulation/Simulation.h
@@ -1,7 +1,7 @@
 #ifndef SIMULATION_H
 #define SIMULATION_H
 
-#include <type_traits>
+#include "../../rlib/rlib.h"
 
 /*
  * Represents the result of a simulation.
@@ -26,7 +26,7 @@ public:
     template<typename T>
     bool getResult(T& _outResult) 
     {
-        static_assert(std::is_base_of_v<SimulationResult, T>, "T must be a type derived from SimulationResult");
+        static_assert(rlib::isDerivedFrom<SimulationResult, T>(), "T must be a type derived from SimulationResult");
         return _getResultInternal(_outResult);
     }
 
